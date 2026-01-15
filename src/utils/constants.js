e556d151ba4819ccbc7844c95ef43770e4a0c093
Shadowww9858 <dk0700008@gmail.com>
2026-01-15 22:49:51 +0530

    Change logo URL and adjust GPT query results count
    Updated logo URL and modified GPT query to return 10 results instead of 5.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,29 +1,22 @@
-export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-
+// Orizon Branding
+export const LOGO = "https://i.ibb.co/LzNf0Ym/orizon-logo-orange.png" 
 export const BACKDROP = '/bg.jpg'
-
-// export const BACKDROP = 'https://assets.nflxext.com/ffe/siteui/vlv3/024bfb98-9e9d-441d-9c5f-d2c798d8e42f/web/IN-en-20251013-TRIFECTA-perspective_b6324826-0535-47c5-be5f-4f50e9663eaf_large.jpg'
-
 export const DP = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt0RN1rfgQ3wvx01TOc_5dyRGoW8SRGWTtmg&s"
 
+// TMDB API Configurations
 export const TMDB_MOVIE_LIST_TYPES = ["Now Playing", "Popular", "Top Rated"]
-
 export const TMDB_MOVIE_URL = (movieType) => `https://api.themoviedb.org/3/movie/${movieType.toLowerCase().replace(' ', '_')}?page=1`
-
 export const TMDB_MOVIE_SEARCH = ({name,year}) => `https://api.themoviedb.org/3/search/movie?query=${name}&include_adult=false&page=1&year=${year}`
-
 export const TMDB_MOVIE_VIDEO_URL = (id) => `https://api.themoviedb.org/3/movie/${id}/videos`
 
 export const TMDB_SERIES_LIST_TYPES = ["Airing Today", "Popular", "Top Rated"]
-
 export const TMDB_SERIES_URL = (seriesType) => `https://api.themoviedb.org/3/tv/${seriesType.toLowerCase().replace(' ', '_')}?page=1`
-
 export const TMDB_SERIES_SEARCH = ({name,year}) => `https://api.themoviedb.org/3/search/tv?query=${name}&include_adult=false&page=1&year=${year}`
-
 export const TMDB_SERIES_VIDEO_URL = (id) => `https://api.themoviedb.org/3/tv/${id}/videos`
 
 export const TMDB_IMAGE_URL = (path) => `https://image.tmdb.org/t/p/w500${path}`
 
+// Video Player Config
 export const YOUTUBE_EMBEDDED_URL = (key) => `https://www.youtube.com/embed/${key}?&amp;controls=0&modestbranding=1&disablekb=1&fs=0&rel=0&iv_load_policy=3&autoplay=1&mute=1&loop=1&playlist=${key}`
 
 export const API_OPTIONS = {
@@ -34,24 +27,11 @@ export const API_OPTIONS = {
     }
 };
 
+// AI Engine (Gemini) - Updated to 10 results
 export const GEMINI_KEY = "AIzaSyCu3QFU_y6uRn61zPAzRbSxXo5Y7FPXJs4";
 
 export const GPT_QUERY = 
   "Act as a Orizon Movie Recommendation system. Query: ";
 
 export const GPT_QUERY_SECONDARY = 
-  ". Only give me names of 5 movies or series, comma separated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
-
-
-
-// LOGO
-// {/* <svg className="fill-current text-red-600" >
-//     <path d="M105.06233,14.2806261 L110.999156,30 C109.249227,29.7497422 107.500234,29.4366857 105.718437,29.1554972 L102.374168,20.4686475 L98.9371075,28.4375293 C97.2499766,28.1563408 95.5928391,28.061674 93.9057081,27.8432843 L99.9372012,14.0931671 L94.4680851,-5.68434189e-14 L99.5313525,-5.68434189e-14 L102.593495,7.87421502 L105.874965,-5.68434189e-14 L110.999156,-5.68434189e-14 L105.06233,14.2806261 Z M90.4686475,-5.68434189e-14 L85.8749649,-5.68434189e-14 L85.8749649,27.2499766 C87.3746368,27.3437061 88.9371075,27.4055675 90.4686475,27.5930265 L90.4686475,-5.68434189e-14 Z M81.9055207,26.93692 C77.7186241,26.6557316 73.5307901,26.4064111 69.250164,26.3117443 L69.250164,-5.68434189e-14 L73.9366389,-5.68434189e-14 L73.9366389,21.8745899 C76.6248008,21.9373887 79.3120255,22.1557784 81.9055207,22.2804387 L81.9055207,26.93692 Z M64.2496954,10.6561065 L64.2496954,15.3435186 L57.8442216,15.3435186 L57.8442216,25.9996251 L53.2186709,25.9996251 L53.2186709,-5.68434189e-14 L66.3436123,-5.68434189e-14 L66.3436123,4.68741213 L57.8442216,4.68741213 L57.8442216,10.6561065 L64.2496954,10.6561065 Z M45.3435186,4.68741213 L45.3435186,26.2498828 C43.7810479,26.2498828 42.1876465,26.2498828 40.6561065,26.3117443 L40.6561065,4.68741213 L35.8121661,4.68741213 L35.8121661,-5.68434189e-14 L50.2183897,-5.68434189e-14 L50.2183897,4.68741213 L45.3435186,4.68741213 Z M30.749836,15.5928391 C28.687787,15.5928391 26.2498828,15.5928391 24.4999531,15.6875059 L24.4999531,22.6562939 C27.2499766,22.4678976 30,22.2495079 32.7809542,22.1557784 L32.7809542,26.6557316 L19.812541,27.6876933 L19.812541,-5.68434189e-14 L32.7809542,-5.68434189e-14 L32.7809542,4.68741213 L24.4999531,4.68741213 L24.4999531,10.9991564 C26.3126816,10.9991564 29.0936358,10.9054269 30.749836,10.9054269 L30.749836,15.5928391 Z M4.78114163,12.9684132 L4.78114163,29.3429562 C3.09401069,29.5313525 1.59340144,29.7497422 0,30 L0,-5.68434189e-14 L4.4690224,-5.68434189e-14 L10.562377,17.0315868 L10.562377,-5.68434189e-14 L15.2497891,-5.68434189e-14 L15.2497891,28.061674 C13.5935889,28.3437998 11.906458,28.4375293 10.1246602,28.6868498 L4.78114163,12.9684132 Z"></path>
-// </svg> */}
-
-
-// BACKGROUND
-// {/* <img src="https://assets.nflxext.com/ffe/siteui/vlv3/024bfb98-9e9d-441d-9c5f-d2c798d8e42f/web/IN-en-20251013-TRIFECTA-perspective_b6324826-0535-47c5-be5f-4f50e9663eaf_large.jpg"
-// srcset="https://assets.nflxext.com/ffe/siteui/vlv3/024bfb98-9e9d-441d-9c5f-d2c798d8e42f/web/IN-en-20251013-TRIFECTA-perspective_b6324826-0535-47c5-be5f-4f50e9663eaf_large.jpg 2000w, https://assets.nflxext.com/ffe/siteui/vlv3/024bfb98-9e9d-441d-9c5f-d2c798d8e42f/web/IN-en-20251013-TRIFECTA-perspective_b6324826-0535-47c5-be5f-4f50e9663eaf_medium.jpg 1279w, https://assets.nflxext.com/ffe/siteui/vlv3/024bfb98-9e9d-441d-9c5f-d2c798d8e42f/web/IN-en-20251013-TRIFECTA-perspective_b6324826-0535-47c5-be5f-4f50e9663eaf_small.jpg 959w"
-// alt="" aria-hidden="true" /> */}
-// {/* The aria-hidden attribute can be used to hide non-interactive content from the accessibility API. */}
+  ". Only give me names of 10 movies or series, comma separated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya, De Dana Dan, Welcome, Hera Pheri, Drishyam, Kantara";
